be64af2e7d3d4e861c59240f23f06a8acae4ceab
Hira Khalid <hiratech88@gmail.com>
2024-10-16 22:23:23 +0500

    show countries dynamic data

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline p-4">Hello World!</h1>
+      <h2 className="text-yellow-500 text-2xl  mt-2 ml-5 m ">
+        My name is Hira Khalid
+      </h2>
+    </div>
+  );
+}
